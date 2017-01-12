02da432baaaa65e933f72da889e88af404ad7bef
waniu <waniu121@gmail.com>
2017-01-12 23:55:49 +0800

    implement posts pagination with will_paginate

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,5 @@ class Post < ApplicationRecord
   belongs_to :group
 
   validates :content, presence: true
-  scope :recent, --> { order"create_at DESC)}
-  
-
+  scope :recent, -> { order("created_at DESC")}
 end
